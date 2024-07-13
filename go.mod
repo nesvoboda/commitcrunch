5669b8097e18e3e9e1bc3c07a95de8fb8a347e5f
Arseniy Shishaev <35579920+nesvoboda@users.noreply.github.com>
2024-07-13 16:37:29 +0200

    Add initial backend

---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/nesvoboda/devcoach
+
+go 1.22.5
+
+require (
+	github.com/Khan/genqlient v0.7.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/tools v0.18.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/charmbracelet/log"
+	"github.com/go-chi/chi/v5"
+)
+
+type handler struct {
+	client graphql.Client
+	cache  cache
+}
+
+type authedTransport struct {
+	key     string
+	wrapped http.RoundTripper
+}
+
+func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", "bearer "+t.key)
+	return t.wrapped.RoundTrip(req)
+}
+
+func main() {
+	log.Info("starting")
+	key := os.Getenv("GITHUB_TOKEN")
+	if key == "" {
+		panic(fmt.Errorf("must set GITHUB_TOKEN=<github token>"))
+	}
+
+	httpClient := http.Client{
+		Transport: &authedTransport{
+			key:     key,
+			wrapped: http.DefaultTransport,
+		},
+	}
+	graphqlClient := graphql.NewClient("https://api.github.com/graphql", &httpClient)
+
+	handler := handler{
+		client: graphqlClient,
+		cache:  newCache(),
+	}
+
+	r := chi.NewRouter()
+
+	r.HandleFunc("/contribs/{username}", func(w http.ResponseWriter, r *http.Request) {
+		username := chi.URLParam(r, "username")
+
+		if username == "" {
+			log.Error("Empty username")
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		log.Info("Request", "username", username)
+
+		days, ok := handler.cache.get(username)
+		var err error
+		if !ok {
+			days, err = handler.getDays(username)
+			if err != nil {
+				log.Error("Error getting contributions", "error", err)
+				http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+				return
+			}
+			handler.cache.set(username, days)
+		}
+
+		bytes, err := json.Marshal(days)
+
+		if err != nil {
+			log.Error("Error marshaling", "error", err)
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.WriteHeader(http.StatusOK)
+		w.Write(bytes)
+		log.Info("Request successful")
+	})
+
+	log.Info("Starting the router")
+	http.ListenAndServe(":8080", r)
+
+}
+
+type day struct {
+	Date     string `json:"date"`
+	Contribs int    `json:"contribs"`
+}
+
+type cache struct {
+	mu   sync.Mutex
+	data map[string][]day
+}
+
+const MAX_CACHE_LEN = 100
+
+func newCache() cache {
+	c := cache{
+		mu:   sync.Mutex{},
+		data: make(map[string][]day, MAX_CACHE_LEN),
+	}
+
+	go func() {
+		time.Sleep(time.Hour)
+		c.clear()
+	}()
+
+	return c
+}
+
+func (c cache) get(key string) ([]day, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.data[key]
+	if ok {
+		log.Info("Cache hit", "key", key)
+	} else {
+		log.Info("Cache miss", "key", key)
+	}
+	return item, ok
+}
+
+func (c cache) set(key string, value []day) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if len(c.data) > MAX_CACHE_LEN {
+		c.data = make(map[string][]day)
+	}
+
+	c.data[key] = value
+}
+
+func (c cache) clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	log.Info("Cache cleared")
+
+	c.data = make(map[string][]day)
+}
+
+func (h handler) getDays(username string) ([]day, error) {
+	ctx := context.Background()
+
+	resp, err := getContributions(ctx, h.client, username)
+
+	if err != nil {
+		return nil, err
+	}
+
+	days := make([]day, len(resp.User.ContributionsCollection.ContributionCalendar.Weeks)*7)
+
+	for i := range resp.User.ContributionsCollection.ContributionCalendar.Weeks {
+		week := resp.User.ContributionsCollection.ContributionCalendar.Weeks[i]
+		for d := range week.ContributionDays {
+			days[i*7+d] = day{
+				Date:     week.ContributionDays[d].Date,
+				Contribs: week.ContributionDays[d].ContributionCount,
+			}
+		}
+	}
+
+	return days, nil
+}
